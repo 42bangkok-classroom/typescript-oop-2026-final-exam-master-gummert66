98da6afe65353d8bea47c00f176c95b1bd94f520
Siwaphon Nithisboonglang <gummert66@gmail.com>
2026-03-25 12:09:56 +0700

    sub

---
+++ src/purchases/purchase.interface.ts
@@ -0,0 +1,11 @@
+export class IPurchase {
+    id: number;
+    customerName: string;
+    purchaseDate: string;
+}
+
+export class IPurchaseitem {
+    productId: number;
+    quantity: number;
+    price: number;
+}
